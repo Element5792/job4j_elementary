ee7e7fe94a76030e9873ed9591a89708eaf89726
Александр <alexandrosipov93@gmail.com>
2021-10-21 19:56:12 +0300

    4.2. Заголовок в файле java [#159036 #217243]

---
+++ src/main/java/ru/job4j/Exercise.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+public class Exercise {
+    public static void main(String[] args) {
+        int age = 18;
+        System.out.println(age);
+        float lenght = 78.0332f;
+        System.out.println(lenght);
+        boolean baby = true;
+        System.out.println(baby);
+
+    }
+}
