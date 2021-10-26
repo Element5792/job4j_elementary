39aa366db3c6ceb3099b1ed803646a9178b2f054
Александр <alexandrosipov93@gmail.com>
2021-10-26 19:48:53 +0300

    4.1. Аргументы [#295521]

--- src/main/java/ru/job4j/ArgMethod.java
+++ src/main/java/ru/job4j/ArgMethod.java
@@ -3,30 +3,17 @@ package ru.job4j;
 public class ArgMethod {
 
         public static void hello(String name, int age) {
-            System.out.println("Hello, " + name + ", age = " + age);
-        }
 
-        public static void hello(String name) {
-            System.out.println("Hello, " + name + ", age = 6");
-
-        }
-
-        public static void hello(int age) {
-            System.out.println("Hello, Job4j, " + "age = " + age);
-
-        }
-
-        public static void hello(int age, String name) {
             System.out.println("Hello, " + name + ", age = " + age);
         }
 
         public static void main(String[] args) {
             String name = "Job4j";
             int age = 6;
-            ru.job4j.ru.job4j.ArgMethod.hello(name, age);
-            ru.job4j.ru.job4j.ArgMethod.hello(age);
-            ru.job4j.ru.job4j.ArgMethod.hello(name);
-            ru.job4j.ru.job4j.ArgMethod.hello(age, name);
+            ru.job4j.ArgMethod.hello(name, age);
+            ru.job4j.ArgMethod.hello(name, age);
+            ru.job4j.ArgMethod.hello(name, age);
+            ru.job4j.ArgMethod.hello(name, age);
 
         }
     }
